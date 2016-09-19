c7bbae166a97c8a48f56cc162f416f34e1a2dfab
Toru Choi <toru@zikto.com>
2016-09-19 15:55:02 +0900

    Dagger2, Butterknife, retrofit, okhttp

--- app/src/main/java/io/toru/daggerexample/UI/MainActivity.java
+++ app/src/main/java/io/toru/daggerexample/UI/MainActivity.java
@@ -1,4 +1,4 @@
-package io.toru.daggerexample.UI;
+package io.toru.daggerexample.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +8,7 @@ import javax.inject.Inject;
 
 import io.toru.daggerexample.R;
 import io.toru.daggerexample.app.MyApplication;
-import io.toru.daggerexample.network.NetworkApi;
+import io.toru.daggerexample.di.module.ToruModule;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,17 +18,20 @@ public class MainActivity extends AppCompatActivity {
     @Inject
     public NetworkApi api;
 
+    @Inject
+    public Toru module;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ((MyApplication)getApplication()).getComponent().inject(this);
-
         boolean isInjected = (api != null);
         if(isInjected){
             if(api.validateUser("Wonyoung", "Choi")){
                 Log.w(TAG, "onCreate: validated!! ");
+                module.testToru();
             }
         }
         else{
